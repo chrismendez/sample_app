6d1e3bd4c9f95ba31c9a4b97f9c9a61911603ac6
Chris Mendez <chrismendez@gmail.com>
2011-05-21 16:36:27 -0500

    Fixed error in title generator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # return a title on a per-page basis
   def title
     base_title = "Rails Tutorial"
-    if @titile.nil?
+    if @title.nil?
       base_title
     else
       "#{base_title} | #{@title}"
